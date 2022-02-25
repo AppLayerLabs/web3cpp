1eb40b241ef572e1763463423e250564c46f31ba
Jean-Lessa <75625278+Jean-Lessa@users.noreply.github.com>
2022-02-25 11:05:07 -0300

    Add initial skeleton for Web3 main class

--- README.md
+++ README.md
@@ -1,1 +1,5 @@
 # web3cpp
+
+Implementation of [Web3](https://web3js.readthedocs.io) in C++.
+
+Requires a compiler that supports **C++17 or above**.
---
+++ src/web3.h
@@ -0,0 +1,17 @@
+#include <string>
+
+// Main/Umbrella class that houses all modules.
+
+public class Web3 {
+  public:
+    //object modules;
+    static std::string version;
+    //static object utils;
+    //object providers;
+    //object givenProvider;
+    //object currentProvider;
+
+    //bool setProvider(object myProvider);
+    //object BatchRequest();
+    //object extend();
+};
