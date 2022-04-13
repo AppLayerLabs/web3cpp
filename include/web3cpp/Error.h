59a31e53aa6fad8f9eb1d1a93944f5f856582513
Jean-Lessa <75625278+Jean-Lessa@users.noreply.github.com>
2022-04-13 13:26:57 -0300

    Fix Error class not compiling

--- include/web3cpp/Error.h
+++ include/web3cpp/Error.h
@@ -1,32 +1,34 @@
 #ifndef ERROR_H
 #define ERROR_H
 
-
 #include <map>
 #include <string>
 
-// Mapping error code -> error string
-const std::map<uint64_t,std::string> errorCodes {
-  {0, "No error"},
-  {999, "Unknown Error"}
-};
+// Class for handling error codes.
 
 class Error {
   private:
+    const std::map<uint64_t,std::string> codeMap {
+      {0, "No error"},
+      {999, "Unknown Error"}
+    };
     uint64_t code;
     std::string message;
     bool isSet = false;
+
   public:
     void setErrorCode(uint64_t errorCode);
     std::string what();
 
-
+    // Operators
     bool operator==(uint64_t errorCode) const {
       return this->code == errorCode;
     }
+
     bool operator!=(uint64_t errorCode) const {
       return this->code != errorCode;
     }
+
     bool operator!() const {
       return this->code != 0;
     }
--- src/Error.cpp
+++ src/Error.cpp
@@ -1,23 +1,20 @@
 #include <web3cpp/Error.h>
 
-
-
-
-class Error {
-  void setErrorCode(uint64_t errorCode) {
-    if (isSet) { throw "Error already set"; };
-    if (errorCodes.contains(errorCode)) {
-      code = errorCode;
-      message = errorCodes[code];
-      isSet = true;
-    } else {
-      code = 999;
-      message = errorCodes[code];
-      isSet = true;
-    }
-  }
-
-  std::string what() {
-    return this->message;
+void Error::setErrorCode(uint64_t errorCode) {
+  if (isSet) { throw "Error already set"; };
+  auto match = codeMap.find(errorCode);
+  if (match != codeMap.end()) {
+    code = match->first;
+    message = match->second;
+    isSet = true;
+  } else {
+    code = 999;
+    message = codeMap.find(code)->second;
+    isSet = true;
   }
 }
+
+std::string Error::what() {
+  return this->message;
+}
+
