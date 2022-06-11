52f4fba511050869ed16d865d8f9b06af65617eb
Jean-Lessa <75625278+Jean-Lessa@users.noreply.github.com>
2022-06-11 17:04:58 -0300

    DB: add tests

--- tests/main.cpp
+++ tests/main.cpp
@@ -5,14 +5,15 @@ int main(int argc, char* argv[]) {
   std::vector<std::string> items;
   if (argc == 1) {
     std::cout << "Usage: " << argv[0] << " all|[CLASS]..." << std::endl
-      << "Available classes: bip39 solidity utils wallet contract" << std::endl;
+      << "Available classes: bip39 database solidity utils wallet contract" << std::endl;
     return 0;
   } else if (argc == 2 && strcmp(argv[1], "all") == 0) {
-    items = {"bip39", "solidity", "utils", "wallet", "contract"};
+    items = {"bip39", "database", "solidity", "utils", "wallet", "contract"};
   } else {
     for (int i = 1; i < argc; i++) {
       if (
         strcmp(argv[i], "bip39") == 0 ||
+        strcmp(argv[i], "database") == 0 ||
         strcmp(argv[i], "solidity") == 0 ||
         strcmp(argv[i], "utils") == 0 ||
         strcmp(argv[i], "wallet") == 0 ||
@@ -24,7 +25,7 @@ int main(int argc, char* argv[]) {
   }
   if (items.empty()) {
     std::cout << "No classes selected." << std::endl
-      << "Available classes: bip39 solidity utils wallet contract" << std::endl;
+      << "Available classes: bip39 database solidity utils wallet contract" << std::endl;
     return 0;
   }
 
@@ -39,6 +40,9 @@ int main(int argc, char* argv[]) {
       t.testKeyPairGeneration();
       t.testMnemonicWordValidity();
       t.testMnemonicAddressGeneration();
+    } else if (item == "database") {
+      std::cout << "* Testing functions from Database" << std::endl;
+      t.testDatabaseCRUD();
     } else if (item == "solidity") {
       std::cout << "* Testing functions from Solidity" << std::endl;
       t.testTypeChecks();
