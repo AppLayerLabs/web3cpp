94e94a220520eebdae19775a3d0a561620167d5b
itamarcps <32653934+itamarcps@users.noreply.github.com>
2023-01-08 21:10:53 -0300

    Revert "Clean off few libraries from CMake"
    This reverts commit 3852625988f9e8cf035607c5ed8eb00fa06dc326.

---
+++ cmake/ProjectCryptoPP.cmake
@@ -0,0 +1,37 @@
+include(ExternalProject)
+
+if (MSVC)
+  set(_only_release_configuration -DCMAKE_CONFIGURATION_TYPES=Release)
+endif()
+
+set(prefix "${CMAKE_BINARY_DIR}/deps")
+set(CRYPTOPP_ROOT_DIR "${prefix}")
+set(CRYPTOPP_LIBRARY "${prefix}/lib/${CMAKE_STATIC_LIBRARY_PREFIX}cryptopp${CMAKE_STATIC_LIBRARY_SUFFIX}")
+set(CRYPTOPP_INCLUDE_DIR "${prefix}/include")
+
+ExternalProject_Add(
+  CryptoPP
+  PREFIX "${prefix}"
+  DOWNLOAD_NAME cryptopp-8.2.0.tar.gz
+  DOWNLOAD_NO_PROGRESS 1
+  URL https://github.com/weidai11/cryptopp/releases/download/CRYPTOPP_8_7_0/cryptopp870.zip
+  URL_HASH SHA256=d0d3a28fcb5a1f6ed66b3adf57ecfaed234a7e194e42be465c2ba70c744538dd
+  UPDATE_DISCONNECTED true
+  BUILD_IN_SOURCE true
+  CMAKE_ARGS -DNDEBUG
+  CONFIGURE_COMMAND ""
+  BUILD_COMMAND make static PREFIX=${prefix} CXXFLAGS=-std=c++${CMAKE_CXX_STANDARD} -j8
+  INSTALL_COMMAND make install-lib PREFIX=${prefix}
+  DOWNLOAD_EXTRACT_TIMESTAMP 1
+  LOG_CONFIGURE 1
+  LOG_BUILD 1
+  LOG_INSTALL 1
+  BUILD_BYPRODUCTS "${CRYPTOPP_LIBRARY}"
+)
+
+add_library(cryptopp STATIC IMPORTED GLOBAL)
+file(MAKE_DIRECTORY "${CRYPTOPP_INCLUDE_DIR}")  # Must exist.
+set_property(TARGET cryptopp PROPERTY IMPORTED_CONFIGURATIONS Release)
+set_property(TARGET cryptopp PROPERTY IMPORTED_LOCATION_RELEASE "${CRYPTOPP_LIBRARY}")
+set_property(TARGET cryptopp PROPERTY INTERFACE_INCLUDE_DIRECTORIES "${CRYPTOPP_INCLUDE_DIR}")
+add_dependencies(cryptopp CryptoPP)
